0ac6232cac560881c8498f59f0a4d32f03cbb904
eldad <eldad@arpalus.com>
2021-07-26 14:31:22 +0300

    start working on string verifcation

---
+++ J_Task/string_annotations_verfication.py
@@ -0,0 +1,19 @@
+import os
+import re
+import glob
+import shutil 
+import pandas as pd 
+
+
+def main(): 
+    
+    VALID_ANNOTATIONS_FILE_PATH = ''
+    
+
+    pass 
+
+if __name__ == "__main__":
+
+    main()
+
+    #perfect match / only perfect SKU / perfect match case sensitive ? / 
