2d9e3ab5bc969c40c286c34d307265f9b8eeba1b
eldad <eldad@arpalus.com>
2021-08-22 00:41:38 +0300

    .

--- Mapping_Files/deployment_evaluation_results.py
+++ Mapping_Files/deployment_evaluation_results.py
@@ -43,12 +43,21 @@ def load_df_from_csv(csv_file_path:str, sep:str = ',') -> pd.DataFrame :
 def download_and_rename(evaluation_file_paths,RESULTS_DIR_PATH):
 
     for file_path in evaluation_file_paths :
+
+
         model_name = os.path.split(file_path)[0]
         model_name = os.path.split(model_name)[-1]
         os.system(f'gsutil -m cp {file_path} {RESULTS_DIR_PATH}')
         src_path = os.path.join(RESULTS_DIR_PATH,'evaluation.csv')
         dist_path = os.path.join(RESULTS_DIR_PATH,f'{model_name}'+'.csv')
-        os.rename(src_path,dist_path)
+
+        log_file_path = os.path.join(RESULTS_DIR_PATH,'logger.txt')
+        with open(f'{log_file_path}', 'a') as f: 
+            try: 
+                os.rename(src_path,dist_path)
+            except Exception as e : 
+                f.write(f'\n{e}\n')
+
 
 
 def main(): 
@@ -72,7 +81,7 @@ def main():
     evaluation_file_paths = [os.path.join(path , f'{model_name}' + '_results') for path,model_name in zip(evaluation_file_paths,models_list)]
     evaluation_file_paths = [os.path.join(path , 'evaluation.csv') for path in evaluation_file_paths]  
     
-    download_and_rename(evaluation_file_paths,RESULTS_DIR_PATH)
+    # download_and_rename(evaluation_file_paths,RESULTS_DIR_PATH)
     
 if __name__ == "__main__":
     
